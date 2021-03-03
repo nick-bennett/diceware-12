15ed440330497b9ef81eeb02675dac2dacc8baeb
Nick Bennett <nick@nickbenn.com>
2021-03-03 11:38:06 -0700

    Serialize Word as a String.

---
+++ src/main/java/edu/cnm/deepdive/diceware/configuration/ExceptionMapping.java
@@ -0,0 +1,18 @@
+package edu.cnm.deepdive.diceware.configuration;
+
+import java.util.NoSuchElementException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionMapping {
+
+  @ExceptionHandler(NoSuchElementException.class)
+  @ResponseStatus(HttpStatus.NOT_FOUND)
+  public void resourceNotFound() {
+    // TODO Provide more information to consumer.
+  }
+
+}
---
+++ src/main/java/edu/cnm/deepdive/diceware/controller/SimpleDicewareController.java
@@ -0,0 +1,32 @@
+package edu.cnm.deepdive.diceware.controller;
+
+import edu.cnm.deepdive.diceware.model.dao.PassphraseRepository;
+import edu.cnm.deepdive.diceware.model.entity.Passphrase;
+import edu.cnm.deepdive.diceware.model.entity.Word;
+import edu.cnm.deepdive.diceware.service.PassphraseGenerator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/diceware")
+public class SimpleDicewareController {
+
+  private final PassphraseGenerator generator;
+
+  @Autowired
+  public SimpleDicewareController(PassphraseGenerator generator) {
+    this.generator = generator;
+  }
+
+  @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+  public String[] get(@RequestParam(required = false, defaultValue = "4") int length) {
+    return generator.generate(length);
+  }
+
+}
